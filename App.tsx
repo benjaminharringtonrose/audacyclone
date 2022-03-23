b73505f9541bdcf9aff38defd00dca929f80c70f
benjamin <benjaminharringtonrose@gmail.com>
2022-03-23 17:52:44 -0400

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/screens/index.tsx
@@ -0,0 +1,13 @@
+import { HomeScreen } from './HomeScreen';
+import { ExploreScreen } from './ExploreScreen';
+import { PlayerScreen } from './PlayerScreen';
+import { PodcastsScreen } from './PodcastsScreen';
+import { SettingsScreen } from './SettingsScreen';
+
+export {
+  HomeScreen,
+  ExploreScreen,
+  PlayerScreen,
+  PodcastsScreen,
+  SettingsScreen,
+};
