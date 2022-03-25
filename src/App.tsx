e7e2c7371dc3376959b56f55202858acf385d5ff
benjamin <benjaminharringtonrose@gmail.com>
2022-03-25 08:28:25 -0400

    add console.warn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import { LogBox } from 'react-native';
 import { Provider } from 'react-redux';
 import { MainStack } from './navigation';
 import { StatusBar } from 'react-native';
 import { store } from './store';
+
+LogBox.ignoreLogs(['[react-native-gesture-handler]']);
 
 const App = () => {
   return (
